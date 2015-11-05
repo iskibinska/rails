f77f8304236836ce9df9d74a41cc9d83f2726dda
Izabela Skibinska <iskibinska@harryfox.com>
2015-11-04 19:44:52 -0500

    rails

--- simple_cms/config/routes.rb
+++ simple_cms/config/routes.rb
@@ -1,4 +1,7 @@
 SimpleCms::Application.routes.draw do
+  root "demo/index"
+
+  match ':controller(/:action(/:id))', :via => :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
